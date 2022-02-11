98c950ddbfd7b7ae35894c6808bf6388933ea472
Emilie <adamanska@gmail.com>
2022-02-11 15:02:18 +0100

    add tc

--- src/routes/uxinterviews.svelte
+++ src/routes/uxinterviews.svelte
@@ -1,5 +1,6 @@
 <script>
   import Casco from "../components/interviews/casco.svelte"
+  import TC from "../components/interviews/tc.svelte"
 </script>
 
 <h2 class="big-title mb-4">UX interviews</h2>
@@ -7,3 +8,4 @@
 <p class="opacity-50 mb-8">User interviews are where a researcher asks questions of, and records responses from, users. They can be used to examine the user experience, the usability of the product or to flesh out demographic or ethnographic data (for input into user personas) among many other things.</p>
 
 <Casco/>
+<TC/>
