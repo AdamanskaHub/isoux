34cf6dfe6b2e4945a2ee8a60b173493d5d8a5768
Emilie <adamanska@gmail.com>
2022-05-12 15:11:35 +0200

    test routing

--- src/routes/uxthoughts.svelte
+++ src/routes/uxthoughts.svelte
@@ -1,5 +1,17 @@
 <script>
   import NewsThoughts from "../components/thoughts/newsthoughts.svelte"
+  // import { onMount } from 'svelte';
+
+  // onMount(()=>{
+  //   const mobileBtn =  document.querySelector('.mobile-menu')
+  //   const sidebar= document.querySelector('.sidebar')
+  //   mobileBtn.addEventListener('click', () => {
+  //   sidebar.classList.toggle('-translate-x-full')
+  //   })
+  // })
+  
+  // import {page} from '$app/stores'
+
 </script>
 
 <h2 class="big-title mb-4">UX thoughts</h2>
@@ -7,4 +19,4 @@
 <p class="opacity-50 mb-8">A collection of ideas on how the user experience could be improved at a more strategic level. An approach free of the limits of budget, habits, hierarchy, etc, that only looks at the objectives of ISO and its users. </p>
 
 <NewsThoughts/>
-<a href={'/uxwebsite'}>Website criticals</a>
+<a href="/uxwebsite" class="font-bold">Website criticals</a>
