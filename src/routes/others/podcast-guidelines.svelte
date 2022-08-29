3a0e61bfc8d63615bc66a32269ae44bdbbc1b6c0
Emilie <adamanska@gmail.com>
2022-08-29 12:06:36 +0200

    podcast

--- src/routes/others/podcast-guidelines.svelte
+++ src/routes/others/podcast-guidelines.svelte
@@ -1,2 +1,36 @@
-<h3>Title</h3>
-<p>Para</p>
+
+
+<h1>Podcast guidelines</h1>
+
+<h3>First off</h3>
+
+<p>Remember who's your audience. Do they want long in-depth pieces, short to the point? Will they share it? What's their level of knowledge?</p>
+<p>What are you trying to achieve? Educating people with zero knowledge on the existence of std 1234? Or Showing experts that ISO can go deep on some subjects?</p>
+<p>How will you promote your piece of content? Is the timing right? How many views do you expect? Can you target well your audience or is it a shot in the dark?</p>
+
+
+<h3>Style</h3>
+
+<p>Intro - We want to harmonize the intro.</p>
+<p>Tools - We use good mics for quality sound. Of course we can't control the interviewee's mic. We use riverside to record the podcast. Ask Emilie or Liz or Vivienne for the access.</p>
+<p>Video - If possible have a video recording of your podcast. It uses a neutral background and has a small iso logo on the bottom right of the screen (to be added post recording in riverside).</p>
+<p>Background - In ISO one of the M rooms has a blue wall that works quite well and has already been used.</p>
+<p>DON'T use the effect that blurs your background or replaces it with something else!!! Just go somewhere where the background is nice.</p>
+
+<h3>Content</h3>
+
+<p>Length - try to keep it around 15-20min. However, if you think you've found something interesting that can be 5min or 1h30 long, there might be an audience for it. Diversity is important.</p>
+<p>Listen to other podcasts to get an idea of what is already being done, it's the easiest way.</p>
+<p>Don't read text, we can hear when the text is read.</p>
+<p>Be relaxed. If you mess up it's ok, you re-record ;) </p>
+
+<h3>Tips</h3>
+
+<p>Test that you can hear the other person before you start recording. Have a one minute chat to make sure the sound is good on both ends, then when ready hit record.</p>
+<p>Make sure no one will interrupt you.</p>
+<p>Put on silent all phone and computers in the room you record from to not have notifications beeps.</p>
+<p>Have a constant light source (careful with natural light on a cloudy day for instance).</p>
+<p>Make sure to have a nice background.</p>
+<p>Adjust your camera so that it is at eye level ideally.</p>
+<p>Smile when you talk. Even if there's no camera.</p>
+<p>Do your best and don't hesitate to ask for help ;) </p>
